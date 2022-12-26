3e5174028995d27efce59e376884d80a645b354e
derickdecesare <deerriicckk@gmail.com>
2022-12-26 14:30:14 -0800

    fixed type filter outline

--- components/TypeSelect.js
+++ components/TypeSelect.js
@@ -14,13 +14,13 @@ export default function TypeSelect({type, setType, setChatHistory, setMessage, s
 
   return (
     <div className=' flex flex-row justify-center' >
-      <label htmlFor="site" className="block text-sm font-medium text-gray-900">
+      <label htmlFor="site" className="block text-sm font-medium  text-gray-900">
         
       </label>
       <select
         id="site"
         name="site"
-        className=' block sm:w-half w-half rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm'
+        className=' block sm:w-half outline outline-gray-500 w-half rounded-md focus:outline-indigo-500 py-2 pl-3 pr-10 text-base focus:border-blue-600 focus:ring-indigo-500 sm:text-sm'
         value={type}
         
         onChange={setTypeRefresh}
